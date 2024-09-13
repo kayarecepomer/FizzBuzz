d79e08cb6c6770f998f98e81b720f4e42a064c86
Kaya <omer1935kaya@gmail.com>
2024-09-13 01:28:25 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                number++;
+            }
+        }
+        System.out.println("multiples is: " + number);
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+
+    public static void main(String[] args) {
+        int n = 100;
+        int step_cnt = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+            step_cnt++;
+        }
+        System.out.println("steps number is : " + step_cnt);
+    }
 }
